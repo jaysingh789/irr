25b1ab01fa87b0d5f6bfb6b5330241c583cf8116
JAYSINGH-DESKTO\jaysingh <jaysingh18587@gmail.com>
2023-07-26 19:21:19 +0530

    version 0.0.3

---
+++ src/Facade/irr.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace codetimer\irr\Facade;
+
+use Illuminate\Support\Facades\Facade as IlluminateFacade;
+
+/**
+ * @method static \Barryvdh\DomPDF\PDF setPaper($paper, $orientation = 'portrait')
+ * @method static \Barryvdh\DomPDF\PDF setBaseHost(string $baseHost)
+ * @method static \Barryvdh\DomPDF\PDF setProtocol(string $protocol)
+ * @method static \Barryvdh\DomPDF\PDF setHttpContext($httpContext)
+ * @method static \Barryvdh\DomPDF\PDF setCallbacks(array $callbacks)
+ * @method static \Barryvdh\DomPDF\PDF setWarnings($warnings)
+ * @method static \Barryvdh\DomPDF\PDF setOption(array|string $attribute, $value = null)
+ * @method static \Barryvdh\DomPDF\PDF setOptions(array $options)
+ * @method static \Barryvdh\DomPDF\PDF loadView($view, $data = array(), $mergeData = array(), $encoding = null)
+ * @method static \Barryvdh\DomPDF\PDF loadHTML($string, $encoding = null)
+ * @method static \Barryvdh\DomPDF\PDF loadFile($file)
+ * @method static \Barryvdh\DomPDF\PDF addInfo($info)
+ * @method static string output($options = [])
+ * @method static \Barryvdh\DomPDF\PDF save()
+ * @method static \Illuminate\Http\Response download($filename = 'document.pdf')
+ * @method static \Illuminate\Http\Response stream($filename = 'document.pdf')
+ *
+ */
+class irr extends IlluminateFacade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'IRR';
+    }
+
+
+}
